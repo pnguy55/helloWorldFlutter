030f1fde0cf163b2b91b44389e86c3b2584ad43d
Pnguy55 <pnguy55@gmail.com>
2018-05-31 13:39:17 -0500

    Hello world.

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    new Center(
+      child: new Text(
+        'Hello, world!',
+        textDirection: TextDirection.ltr,
+      )
+
+    )
+  );
+
+}
